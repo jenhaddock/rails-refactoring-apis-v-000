4db99a2858d9b44a1aac616a99511b4969b6d701
Jennifer Haddock <jentx123@hotmail.com>
2017-12-04 00:16:24 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -7,7 +7,7 @@ class GithubService
   end
 
   def authenticate!(client_id, client_secret, code)
-    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['code'] = code
